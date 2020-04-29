e0573f59a39ff9b6aa7660941f770df69287b167
sangeeth98 <sangeethkumarvm@gmail.com>
2020-04-29 19:16:49 +0530

    added new experimental codes

---
+++ Experimental-Codes/face_detector.py
@@ -0,0 +1,17 @@
+import cv2, dlib, pickle
+import pickle
+
+cap = cv2.VideoCapture(0)
+detector = dlib.get_frontal_face_detector()
+while(True):
+    _, frame = cap.read()
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    dets = detector(rgb, 1)
+    for i, d in enumerate(dets):
+        cv2.rectangle(frame, (d.left(), d.top()), (d.right(), d.bottom()), (0, 255, 0), 2)
+    cv2.imshow('Frame', frame)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key==ord('q'): break
+
+cv2.destroyAllWindows()
